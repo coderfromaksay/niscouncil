224bc5aae71a4484ee32ac7827e5ab18fb012d5b
coderfromaksay <86477162+coderfromaksay@users.noreply.github.com>
2024-11-12 19:49:24 +0500

    dasdas

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var deploy = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("homePage/public")
+  return gulp.src("./dist/homePage/public")
     .pipe(deploy())
 });
 
