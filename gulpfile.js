bb43f23cddab2bd0d3eb0bd95c658d26bb0db0d9
Velzy <105767300+Mrhrsk@users.noreply.github.com>
2024-11-29 12:29:07 +0500

    kdjdkfj

--- gulpfile.js
+++ gulpfile.js
@@ -100,5 +100,5 @@ gulp.task('default', gulp.series(
 // Build task
 gulp.task('build', gulp.series(
     gulp.parallel('clean:css', 'clean:js', 'clean:html'),
-    gulp.parallel('sass', 'js', 'ht')
+    gulp.parallel('sass', 'js', 'html')
 ));
