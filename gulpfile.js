c157632b036b1ea35ef83b35be1bbb5f140a8736
Velzy <105767300+Mrhrsk@users.noreply.github.com>
2024-11-29 15:15:36 +0500

    new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,6 @@ gulp.task('sass', function () {
       .pipe(cleancss())
       .pipe(rename({ suffix: '.min' }))
       .pipe(gulp.dest(paths.dist.css))
-      .pipe(browserSync.stream());
 });
 
 // JavaScript Minification
@@ -64,14 +63,12 @@ gulp.task('js', function () {
         message: '<%= error.message %>',
       })))
       .pipe(gulp.dest(paths.dist.js))
-      .pipe(browserSync.stream());
 });
 
 // HTML Copy
 gulp.task('html', function () {
   return gulp.src(paths.src.html)
       .pipe(gulp.dest(paths.dist.html))
-      .pipe(browserSync.stream());
 });
 
 // BrowserSync
@@ -85,6 +82,9 @@ gulp.task('browserSync', function () {
     startPath: 'home.html',
   });
   browserSync.watch('dist/**/*.*').on('change',browserSync.reload);
+});
+
+gulp.task('watchfiles', function () {
   gulp.watch(paths.src.scss, gulp.series('sass'));
   gulp.watch(paths.src.js, gulp.series('js'));
   gulp.watch(paths.src.html, gulp.series('html'));
@@ -94,7 +94,7 @@ gulp.task('browserSync', function () {
 gulp.task('default', gulp.series(
     gulp.parallel('clean:css', 'clean:js', 'clean:html'),
     gulp.parallel('sass', 'js', 'html'),
-    'browserSync'
+    gulp.parallel('watchfiles', 'browserSync')
 ));
 
 // Build task
